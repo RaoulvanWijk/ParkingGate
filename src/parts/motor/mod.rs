21e82e9c62f1f35d58cdab64300106560a76a29c
RaoulvanWijk <raoul.van.wijk05@gmail.com>
2023-12-15 20:18:45 +0100

    WIP

--- src/parts/motor/mod.rs
+++ src/parts/motor/mod.rs
@@ -2,14 +2,12 @@ use rust_gpiozero::Servo;
 
 const SERVO_PIN: u8 = 14;
 
-pub mod motor {
-  pub fn open() {
-    let servo = Servo::new(SERVO_PIN);
+pub fn open() {
+    let mut servo = Servo::new(SERVO_PIN);
     servo.max();
-  }
+}
 
-  pub fn close() {
-    let servo = Servo::new(SERVO_PIN);
+pub fn close() {
+    let mut servo = Servo::new(SERVO_PIN);
     servo.min();
-  }
 }
