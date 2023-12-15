4cd5b7e3363c6769cb4e4dd4c3801d0ef47bd1c7
RaoulvanWijk <raoul.van.wijk05@gmail.com>
2023-12-15 20:09:46 +0100

    will it work

--- src/parts/mod.rs
+++ src/parts/mod.rs
@@ -1,8 +1,8 @@
 pub mod motor;
 pub mod sensor;
 
-mod parts {
-    pub fn test() {
-        println!("test");
-    }
-}
+// mod parts {
+//     pub fn test() {
+//         println!("test");
+//     }
+// }
