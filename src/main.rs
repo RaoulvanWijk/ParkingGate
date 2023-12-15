93c59e0a992a42c2a2886c032a2d58c27bd164b1
RaoulvanWijk <raoul.van.wijk05@gmail.com>
2023-12-15 20:31:02 +0100

    test

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,13 @@
 mod parts;
+use rust_gpiozero::Servo;
 use std::{thread, time};
+const SERVO_PIN: u8 = 14;
 
 fn main() {
-    parts::motor::open();
+    // parts::motor::open();
+    let mut servo = Servo::new(SERVO_PIN);
+    servo.max();
     let ten_millis = time::Duration::from_millis(500);
     thread::sleep(ten_millis);
-    parts::motor::close();
+    servo.min();
 }
